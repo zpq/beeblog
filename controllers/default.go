827cd35c7dc8c69cf2f601b6b67e45495cb212b0
zpq <307154456@qq.com>
2016-02-25 11:09:14 +0800

    test modify

--- controllers/default.go
+++ controllers/default.go
@@ -11,6 +11,6 @@ type MainController struct {
 func (c *MainController) Get() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
-	c.Data["message"] = "Hello World!"
+	c.Data["message"] = "Hello World!Beego!"
 	c.TplName = "index.tpl"
 }
